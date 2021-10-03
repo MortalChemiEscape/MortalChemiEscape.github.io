7a32e1baca54b263a24666808b3654a2d7e0ba04
mrpine1 <mikewizstudies@gmail.com>
2021-10-03 18:58:38 -0300

    Added basic files

---
+++ js/index.js
@@ -0,0 +1,119 @@
+//CARTA 4
+function check_answeri(){
+    if (document.forms_de_teste1.input1.value.toLowerCase() == "cobre" && document.forms_de_teste1.input2.value.toLowerCase() == "níquel") {
+      window.alert("CORRECT!");
+      window.open("4Vwin.html", '_blank').focus();
+    }
+    else {
+      window.alert("INCORRECT!");
+    }}
+
+//CARTA 5
+function check_answerii(){
+    if (document.forms_de_teste2.input1.value.toLowerCase() == "1h30" || document.forms_de_teste2.input1.value.toLowerCase() == "1:30") {
+      window.alert("CORRECT!");
+      window.open("5Vwin.html", '_blank').focus();
+        }
+   else {
+      window.alert("INCORRECT!");
+   }}
+
+//CARTA 7
+function check_answeriii(){
+   if (document.forms_de_teste3.input1.value.toLowerCase() == "823543" || document.forms_de_teste3.input1.value.toLowerCase() == "823.543") {
+      window.alert("CORRECT!");
+      window.open("7Vwin.html", '_blank').focus();
+         }
+   else {
+      window.alert("INCORRECT!");
+      }}
+
+//CARTA 15
+function check_answeriv(){
+   if (document.forms_de_teste4.input1.value.toLowerCase() == "mskd3321") {
+      window.alert("CORRECT!");
+      window.open("15Vwin.html", '_blank').focus();
+         }
+   else {
+      window.alert("INCORRECT!");
+      }}
+
+//CARTA 17
+function check_answerv(){
+   if (document.forms_de_teste5.input1.value.toLowerCase() == "media" || document.forms_de_teste5.input1.value.toLowerCase() == "média") {
+      window.alert("CORRECT!");
+      window.open("17Vwin.html", '_blank').focus();
+         }
+   else {
+      window.alert("INCORRECT!");
+      }}
+
+//CARTA 21
+function check_answervi(){
+   if (document.forms_de_teste6.input1.value.toLowerCase() == "37" || document.forms_de_teste6.input1.value.toLowerCase() == 37) {
+      window.alert("CORRECT!");
+      window.open("21Vwin.html", '_blank').focus();
+         }
+   else {
+      window.alert("INCORRECT!");
+      }}
+
+//CARTA 25
+function check_answervii(){
+   if (document.forms_de_teste7.input1.value.toLowerCase() == "860" || document.forms_de_teste7.input1.value.toLowerCase() == 860) {
+      window.alert("CORRECT!");
+      window.open("25Vwin.html", '_blank').focus();
+         }
+   else {
+      window.alert("INCORRECT!");
+      }}
+
+//CARTA 27
+function check_answeriix(){
+   if (document.forms_de_teste8.input1.value.toLowerCase() == "fkndor97") {
+      window.alert("CORRECT!");
+      window.open("27Vwin.html", '_blank').focus();
+         }
+   else {
+      window.alert("INCORRECT!");
+   }}
+
+//CARTA 30
+function check_answerix(){
+   if (document.forms_de_teste9.input1.value.toLowerCase() == "9375" || document.forms_de_teste9.input1.value.toLowerCase() == "93,75" ) {
+      window.alert("CORRECT!");
+      window.open("30Vwin.html", '_blank').focus();
+         }
+   else {
+      window.alert("INCORRECT!");
+   }}
+
+//CARTA 33
+function check_answerx(){
+    if (document.forms_de_teste10.input1.value.toLowerCase() == "óxido de ferro" && document.forms_de_teste10.input2.value.toLowerCase() == "alumínio") {
+      window.alert("CORRECT!");
+      window.open("33Vwin.html", '_blank').focus();
+    }
+    else if (document.forms_de_teste10.input2.value.toLowerCase() == "óxido de ferro" && document.forms_de_teste10.input1.value.toLowerCase() == "alumínio") {
+      window.alert("CORRECT!");
+      window.open("33Vwin.html", '_blank').focus();
+    }
+    else {
+      window.alert("INCORRECT!");
+    }}
+
+//CARTA 34
+function check_answerxi(){
+   if (document.forms_de_teste11.input1.value.toLowerCase() == "225" || document.forms_de_teste11.input1.value.toLowerCase() == "225g" ) {
+      window.alert("CORRECT!");
+      window.open("34Vwin.html", '_blank').focus();
+         }
+   else {
+      window.alert("INCORRECT!");
+   }}
+
+// FUNCÕES DICAS
+
+function carta25_dica2() {
+  window.open("Dica_imgc25/Dica25.html")
+}
